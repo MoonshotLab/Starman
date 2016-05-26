ca0fd0e8be80d2def28e7040c2dcb96df22175a3
Joe Longstreet <joelongstreet@gmail.com>
2016-05-26 13:23:21 -0500

    make an interface that reacts to volume

--- take-off/index.js
+++ take-off/index.js
@@ -1,13 +1,46 @@
-var soundCapture = new Scream.SC({
+var config = {
+  maxVolume : 10,
+  loThreshold : 60,
+  hiThreshold : 80,
   emissionRate : 50
+};
+
+
+// reference some dom elements
+var $ = {
+  volume : document.getElementById('volume'),
+  power  : document.getElementById('power')
+};
+
+
+// create a sound capture instance
+var soundCapture = new Scream.SC({
+  emissionRate : config.emissionRate
 });
 
 
+// startup the device
 Scream.SC.getDevice('Default', function(device){
   soundCapture.listen(device);
 });
 
 
+// listen for volume changes and react
+var powerLevel = 0;
 soundCapture.emitter.addListener('volume', function(volume){
-  console.log(volume);
+  var volumeLevel = volume/config.maxVolume;
+
+  if(volumeLevel < config.hiThreshold && volumeLevel > config.loThreshold){
+    powerLevel++;
+  } else {
+    powerLevel--;
+    if(powerLevel <= 0) powerLevel = 0;
+  }
+
+  $.volume.style.height = volumeLevel + '%';
+  $.power.style.height = powerLevel + '%';
+
+  if(powerLevel >= 100){
+    alert('BLAST OFF!');
+  }
 });
