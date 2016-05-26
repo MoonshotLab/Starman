f45d9c8f53eb7d9c4823a1e933a02001ff4b8beb
Joe Longstreet <joelongstreet@gmail.com>
2016-05-26 17:22:05 -0500

    move utils into init

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -4,3 +4,19 @@ var Scream = {};
 // shims
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+// utils
+class Utils {
+  static get config(){
+    return {
+      maxVolume : 10,
+      loThreshold : 60,
+      hiThreshold : 80,
+      emissionRate : 50
+    };
+  }
+
+  static random(lo, hi) {
+    return Math.floor(Math.random() * (1 + hi - lo)) + lo;
+  }
+}
