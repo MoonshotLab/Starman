af40fc2ae06738d01a07db268445e1c181176c92
Joe Longstreet <joelongstreet@gmail.com>
2016-05-27 10:29:29 -0500

    move sound capture devices to it's own files

---
+++ public/capture/index.js
@@ -0,0 +1,36 @@
+(function(){
+  var socket = io();
+  var queryParams = {};
+
+  // get query paramaters
+  var query = location.search.replace('?', '');
+  var queryArray = query.split('&');
+
+  queryArray.forEach(function(item){
+    var splitz = item.split('=');
+    queryParams[splitz[0]] = splitz[1];
+  });
+
+  if(queryParams.stage){
+    var stage = queryParams.stage;
+
+    // create a sound capture instance
+    var soundCapture = new Scream.SC({
+      emissionRate : Utils.config.emissionRate
+    });
+
+    var deviceName = 'Default';
+    if(stage == 'left') deviceName = 'Default';
+    else if(stage == 'right') deviceName = 'AudioBox USB';
+
+    // startup the device
+    Scream.SC.getDevice(deviceName, function(device){
+      soundCapture.listen(device);
+    });
+
+    // emit the stage left or right sound to the server
+    soundCapture.emitter.addListener('volume', function(volume){
+      socket.emit('stage-' + stage, volume);
+    });
+  }
+})();
