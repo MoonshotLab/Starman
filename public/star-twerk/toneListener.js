93f1da4b8b3678d95ccf1f92257c889564ac18e9
Joe Longstreet <joelongstreet@gmail.com>
2016-06-03 10:45:06 -0500

    write a working tone listner

--- public/star-twerk/toneListener.js
+++ public/star-twerk/toneListener.js
@@ -2,20 +2,23 @@ class ToneListener{
   constructor(){
     var self = this;
 
-    // the current tones being produced
-    this.tones = [];
-    this.toneSet = ['hi', 'lo', 'clap'];
+    // make the tone set available to external classes
+    this.toneSet = ['lo', 'hi', 'clap'];
 
-    // left and right channel data
+    // the current tone being produced
+    this.tone = null;
+
+    // only process tone if wait has been turned off
+    this.wait = false;
+
+    // retain left and right channel data
     this.left  = { volume : 0, frequency : []};
     this.right = { volume : 0, frequency : []};
 
-    // listen to the socket and remember volume
-    // and frequency, determine tones
+    // listen to the socket and remember volume and frequency
     Scream.socket.on('stage-left', function(data){
       self.left = data;
     });
-
     Scream.socket.on('stage-right', function(data){
       self.right = data;
     });
@@ -24,28 +27,58 @@ class ToneListener{
 
 
   getCurrentTone(){
-    // mix the left and right channels
-    var mixed = [];
-    this.left.frequency.forEach(function(value, i){
-      mixed[i] = value;
-    });
-    this.right.frequency.forEach(function(value, i){
-      mixed[i] = mixed[i] + value;
-    });
+    var self = this;
+    var volume = ((this.left.volume + this.right.volume)/2)/Utils.config.sensitivity;
 
-    // find which tone
-    var tone;
-    var max = Math.max(...mixed);
-    var index = mixed.indexOf(max);
-    if(index > Utils.frequencyNodeCount/2){
-      tone = this.toneSet[0];
-    } else if(index < Utils.frequencyNodeCount/2){
-      tone = this.toneSet[1];
-    } else{
-      tone = this.toneSet[2];
+    if(volume > Utils.config.loThreshold && this.wait === false){
+      this.wait = true;
+
+      setTimeout(function(){ self.wait = false; }, 1000);
+
+      var mixed = [];
+      this.left.frequency.forEach(function(value, i){
+        mixed[i] = value;
+      });
+      this.right.frequency.forEach(function(value, i){
+        mixed[i] = (mixed[i] + value)/2;
+      });
+
+      // find the frequency bucket with highest and lowest values
+      var max = Math.max(...mixed);
+      var min = Math.min(...mixed);
+      var maxIndex = mixed.indexOf(max);
+      var minIndex = mixed.indexOf(min);
+
+      // find the average distance from the max
+      var average = 0;
+      mixed.forEach(function(item){
+        average += Math.abs(max - item);
+      });
+      average = Math.round(average/mixed.length);
+
+      // determine tone based on highest and lowest frequency buckets
+      var upperThird = Utils.config.frequencyNodeCount*0.666;
+      var lowerThird = Utils.config.frequencyNodeCount*0.333;
+      if(maxIndex > upperThird && minIndex < lowerThird){
+        this.tone = this.toneSet[1];
+      } else if(maxIndex < lowerThird && minIndex > upperThird){
+        this.tone = this.toneSet[0];
+      } else{
+        this.tone = this.toneSet[2];
+      }
+
+      // log
+      console.log('min        :', min);
+      console.log('max        :', max);
+      console.log('diff       :', max - min);
+      console.log('average    :', average);
+      console.log('min-index  :', minIndex);
+      console.log('max-index  :', maxIndex);
+      console.log('tone       :', this.tone);
+      console.log('--------------');
     }
 
-    return tone;
+    return this.tone;
   }
 
 
